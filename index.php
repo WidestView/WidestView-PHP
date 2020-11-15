4ad064f2edcf6e94edcc1e31a4634a4b96d3f5a0
tamaxx <pedro.tamas@uol.com.br>
2020-11-15 03:10:24 -0300

    Responsive update

--- index.php
+++ index.php
@@ -3,15 +3,20 @@
     <head>
         <meta charset="UTF-8"/>
         <title> Widest View </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" href="imagens/icone_logo_WV.png">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="icon" href="imagens/icone_logo_WV.png"/>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"/>
         <link rel="stylesheet" type="text/css" href="estilo.css" media="screen" />
     </head> 
     <body>
         <?php
         
-        include 'nav-logado1.php';
+        $logged = true;
+        if($logged){
+            include 'nav-logado1.php';
+        } else {
+            include 'nav-deslogado.php';
+        }
         
         ?>
 
@@ -19,41 +24,72 @@
 
         <div id="home">
             
-            <div class="fixed-bg" style="height:55vw;">
-                <video autoplay loop muted playsinline style="width:100%;"></video>
+            <div class="position-relative" id="video-bg" style="height:55vw;">
+                <video id="top-video" autoplay loop muted playsinline style="width:100%; pointer-events: none;"></video>
                 <div style="position:absolute; top:30%; height:15vw; width:40%; background-color: rgba(255, 255, 255, 0.6);"> 
                     <h1 class="ml-5" style="font-size:3.5vw; margin-top:3.1vw;"> Provavelmente o <br> slogan. </h1>
                 </div>
             </div>
 
             <script>
-                let nomes = ['imagens/bgvideos/bgvideo.mp4', 'imagens/bgvideos/bgvideo2.mp4']    
-                let imagem = nomes[Math.floor((Math.random()*nomes.length))];
-                document.querySelector('video').src=imagem;
+                
+                const videos = [
+                    'imagens/bgvideos/bgvideo1.mp4',
+                    'imagens/bgvideos/bgvideo2.mp4',
+                    'imagens/bgvideos/bgvideo3.mp4',
+                    'imagens/bgvideos/bgvideo4.mp4'
+                ]
+                
+                function getNewVideo(){
+                    let lastVideo = localStorage.getItem('lastVideo');
+                    let video = '';
+                    if(lastVideo === null){
+                        video = videos[Math.floor(Math.random() * videos.length)];    
+                    }else{
+                        const index = videos.indexOf(lastVideo);
+                        if (index > -1) {
+                            let availableVideos = videos;
+                            availableVideos.splice(index,1);
+                            video = availableVideos[Math.floor(Math.random() * availableVideos.length)];    
+                        }else{
+                            console.log('Error on localstorage: ' + lastVideo);
+                            video = videos[Math.floor(Math.random() * videos.length)];    
+                        }
+                    }
+                    setVideo(video);
+                }
+                
+                function setVideo(video){
+                    document.getElementById('top-video').src = video; 
+                    localStorage.setItem('lastVideo',video);
+                    console.log(video);
+                }
+                
+                getNewVideo();
             </script>
 
         </div>
   
-        <hr class="my-auto">
+        <hr class="my-auto"/>
 
         <div id="empresa">
 
-            <div class="align-items-center h-100" style="padding-bottom:6vw;">
-                <h2 class="text-center titulo-preto heading"> Empresa </h2>
-                <div class="container text-justify" style="margin-top:5vw;">
+            <div class="align-items-center h-100" style="padding-bottom:2rem;">
+                <h2 class="text-center titulo-preto heading" style="padding-top:7rem"> Empresa </h2>
+                <div class="container text-justify" style="margin-top:5rem;">
                     <div class="row">
-                        <div class="col-4 py-5 px-4">
+                        <div class="col-12 col-lg-4 py-5 px-4">
                             <h3 class="py-2"> Quem somos </h3>
                             <p> <i> A Widest View é um grupo de jovens empreendedores originada ainda em 2020, com o objetivo de prestar serviços na área
                             de desenvolvimento de sistemas no geral. </i> </p>
                         </div>
-                        <div class="col-4 py-5 px-4">
+                        <div class="col-12 col-lg-4 col-md-6 py-5 px-4">
                             <h3 class="py-2">O que fazemos</h3>
                             <p> <i>Trabalhamos com <a href="" style="color:black"> <u> Design Digital </u> </a>, <a href="" style="color:black"> <u> Análise de Sistemas </u> </a>,
                             <a href="" style="color:black"><u>Análise e Desenvolvimento de Banco de Dados</u></a>, Programação <a href="" style="color:black"><u>Front</u></a>
                             e <a href="" style="color:black"> <u> BackEnd </u> </a> e <a href="" style="color:black"> <u> Análise de Infraestrutura </u> </a> </i> <p>
                         </div>
-                        <div class="col-4 py-5 px-4">
+                        <div class="col-12 col-lg-4 col-md-6 py-5 px-4">
                             <h3 class="py-2"> Nossos Objetivos </h3>
                             <p> <i> Alvejamos crescer no mercado da programação e prestação de serviços para os mais diferentes tipos de clientes, e sua 
                             satisfação é temos como objetivo principal </i> <p>
@@ -74,7 +110,7 @@
 
         <div id="portfolio">
 
-            <div class="align-items-center h-100" style="padding-bottom:6vw;">
+            <div class="align-items-center h-100" style="padding-bottom:6rem;">
                 <h2 class="text-center titulo-preto heading"> Portfólio </h2>
                 <p class="text-center" style="margin-top:5rem;"> Esses são alguns de nossos trabalhos </p>
             </div>
@@ -86,27 +122,37 @@
         <div id="contato">
 
             <div class="fixed-bg bg-imagem">
-                <div class="h-100 bg-tema" style="padding-bottom: 6vw; color: #E5E0E0; opacity:0.8">
+                <div class="h-100 bg-tema" style="padding-bottom: 4rem; color: #E5E0E0; opacity:0.8">
                     <h2 class="text-center titulo-branco heading"> Contato </h2>
-                    <div class="container pt-5 pb-1" style="margin-top:5vw;">
-                        <div class="row">
-                            <div class="col-4 px-4">
-                                <i class="fas fa-envelope-open-text" style="font-size: 40px; margin-bottom: 2vw;"> </i>
+                    <div class="container pt-5 pb-1" style="margin-top:4rem;">
+                        <div class="row text-center text-md-left text-lg-left">
+                            <div class="col-12 item-contato col-md-6 col-lg-4 px-4">
+                                <i class="fas fa-envelope-open-text" style="font-size: 35px; margin-bottom: 1rem;"> </i>
                                 <h3> E-mail </h3>
                                 <p> Alguma dúvida? Crítica? Sugestão? Proposta de serviço? Encaminhe para nós! </p>  
+                                <h5 style="color:#8986AC"> widestview@gmail.com </h5>
                             </div>
-                            <div class="col-4 px-4">
-                                <i class="fas fa-phone" style="font-size: 40px; margin-bottom: 2vw;"> </i>
+                            <div class="col-12 item-contato col-md-6 col-lg-4 px-4">
+                                <i class="fas fa-phone" style="font-size: 35px; margin-bottom: 1rem;"> </i>
                                 <h3> Telefone </h3>
                                 <p> Deseja estabelecer um contato direto e mais pessoal com nós? Não hesite em ligar! </p>
+                                <h5 style="color:#8986AC"> (11) 95384-8594 </h5>
                             </div>
-                            <div class="col-4 px-4">
-                                <i class="fas fa-map-marker-alt" style="font-size: 40px; margin-bottom: 2vw;"> </i>
+                            <div class="col-12 col-md-12 col-lg-4 px-4">
+                                <i class="fas fa-map-marker-alt" style="font-size: 35px; margin-bottom: 1rem;"> </i>
                                 <h3> Endereço </h3>
                                 <p> Caso queira fazer uma visita à nossa sede e ver pessoalmente nosso trabalho. </p>
+                                <h5 style="color:#8986AC"> Rua Luiz Scott, 209 </h5>
                             </div>
                         </div>
-                        <div class="row text-center" style="padding-top:5vw;">
+                        <style>
+                            @media(max-width:992px){
+                                .item-contato{
+                                    padding-bottom:4rem;
+                                }
+                            }
+                        </style>
+                        <div class="row text-center" style="padding-top:4rem;">
                             <div class="col text-center">
                                 <h4> Nossas redes sociais: </h4>
                             </div>
@@ -123,7 +169,6 @@
                     </div>
                 </div>
             </div>
-
         </div>
         
         <script type="text/javascript" src="main.js"></script>
@@ -131,7 +176,7 @@
         <script src="https://kit.fontawesome.com/87aae4010f.js" crossorigin="anonymous"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>                   
+
     </body>
 </html>
--- nav-logado1.php
+++ nav-logado1.php
@@ -6,32 +6,33 @@
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav mx-3 ">
-            <li class="nav-item nav-item">
+        <ul class="navbar-nav mx-3">
+            <li class="nav-item d-flex justify-content-center">
                 <a class="nav-link" href="index.php#home">Home <span class="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item ml-4">
+            <li class="nav-item ml-lg-4 d-flex justify-content-center">
                 <a class="nav-link" href="index.php#empresa">Empresa</a>
             </li>
-            <li class="nav-item nav-item ml-4">
+            <li class="nav-item nav-item ml-lg-4 d-flex justify-content-center">
                 <a class="nav-link" href="index.php#portfolio">Portfólio</a>
             </li>
-            <li class="nav-item nav-item ml-4">
+            <li class="nav-item nav-item ml-lg-4 d-flex justify-content-center">
                 <a class="nav-link" href="index.php#contato">Contato</a>
             </li>
-            <li class="nav-item nav-item ml-4">
-                <a class="nav-link" style="color:#9F7E23;" href="admin.php">Administração</a>
+            <li class="nav-item nav-item ml-lg-4 d-flex justify-content-center">
+                <a class="nav-link" style="color:#8986AC;" href="admin.php">Administração</a>
             </li>
         </ul>
-        <div class="dropdown ml-auto mr-3">
-            <img src="imagens/profile.png" style="width:45px;" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-
-            <div class="dropdown-menu dropdown-menu-lg-right mt-4" aria-labelledby="dropdownMenuLink">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-        </div> 
-        <a href="#" class="ml-3 mr-5" style="color:white;"> Sair </a>
+        <div class="d-flex justify-content-center pt-auto ml-lg-auto mb-3 mb-lg-0 mt-3 mt-lg-0">
+            <div class="dropdown mr-4 ">
+                <img src="imagens/profile.png" style="width:45px;" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <div class="dropdown-menu dropdown-menu-lg-right mt-4" aria-labelledby="dropdownMenuLink">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </div> 
+            <a href="#" class="mr-2 mr-lg-5 mt-auto mb-auto" style="color:white;"> Sair </a>
+        </div>
     </div>
 </nav>
