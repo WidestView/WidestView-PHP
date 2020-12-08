20d84b9ff36c2f51e19b977fa98c2763092379fd
tamaxx <pedro.tamas@uol.com.br>
2020-12-08 16:15:54 -0300

    Dashboard update

--- application/views/pages/admin/dashboard.php
+++ application/views/pages/admin/dashboard.php
@@ -10,41 +10,33 @@
         <div class="d-none d-lg-flex col-12 mt-4" style="position:absolute;">
             <a href="/admin/selector" class="text-dark" style="font-size:2rem;"><i id="btn-back" class="fa fa-chevron-circle-left text-white"></i></a>
         </div>
-        <div class="col-10">
+        <div class="col-11 mt-5">
             <div style="margin-top:3rem;">
-                <div class="row">
-                    <div class="col-12 col-lg-2 mx-0 mx-lg-5 mb-5 mb-lg-0">
-                        <div class="row mb-5" style="background-color:white; height:250px;"> 
-                            <p>  </p>
+                <div class="row justify-content-center">
+                    <div class="col-12 col-lg-2 mx-0 mr-lg-5 mb-5 mb-lg-0">
+                        <div class="row mb-4 justify-content-center" style="background-color:white; height:250px;"> 
+                            <i class="fas fa-user-shield text-secondary" style="font-size:6rem; margin-top:5rem;"></i>
+                        </div>
+                        <div class="row mb-4" style="background-color:white; height:80px;">
+                            <h4 class="mt-4 ml-4"> Clientes </h4>
+                            <h4 class="mt-4 mr-5 ml-auto"> 5 </h4>
+                        </div>
+                        <div class="row mb-4" style="background-color:white; height:80px;">
+                            <h4 class="mt-4 ml-4"> Consultores </h4>
+                            <h4 class="mt-4 mr-5 ml-auto"> 7 </h4>
+                        </div>
+                        <div class="row mb-4" style="background-color:white; height:80px;">
+                            <h4 class="mt-4 ml-4"> Demandas </h4>
+                            <h4 class="mt-4 mr-5 ml-auto"> 13 </h4>
                         </div>
                         <div class="row" style="background-color:white; height:80px;">
-                            <h4 class="mt-4 ml-4"> Clientes </h4>
+                            <h4 class="mt-4 ml-4"> Relatórios </h4>
+                            <h4 class="mt-4 mr-5 ml-auto"> 15 </h4>
                         </div>
                     </div>
                     <div class="col-12 col-lg-8 text-center ml-0 ml-lg-3" style="background-color:white; border-radius:10px;">
                         <h6 class="mt-4"> RELAÇÃO DEMANDA X TEMPO </h6>
-                        <canvas id="myChart" height="90"></canvas>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12 col-lg-2 mx-0 mx-lg-5">
-                        <div class="row mt-5 mb-5" style="background-color:white; height:80px"> 
-                            <h4 class="mt-4 ml-4"> Funcionários </h4>
-                        </div>
-                        <div class="row mt-5 mb-5" style="background-color:white; height:80px"> 
-                            <h4 class="mt-4 ml-4"> Serviços </h4>
-                        </div>
-                        <div class="row mt-5" style="background-color:white; height:80px"> 
-                            <h4 class="mt-4 ml-4"> Demandas </h4>
-                        </div>
-                    </div>
-                    <div class="col-12 col-lg-4 mt-5 mr-0 mr-lg-3 text-center" style="background-color:white; border-radius:10px;">
-                        <h6 class="mt-4"> RELAÇÃO FUNCIONÁRIOS X TEMPO </h6>
-                        <canvas id="myChart2" height="160"></canvas>
-                    </div>
-                    <div class="col-12 col-lg-4 mt-5 ml-0 ml-lg-3 text-center" style="background-color:white; border-radius:10px;">
-                        <h6 class="mt-4"> RELAÇÃO FUNCIONÁRIOS / DEMANDAS X TEMPO </h6>
-                        <canvas id="myChart3" height="160"></canvas>
+                        <canvas id="myChart" height="159"></canvas>
                     </div>
                 </div>
             </div>
@@ -96,108 +88,36 @@
 </script>
 
 <script>
+
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho','Julho', 'Agosto', 'Setembro', 'Outubro', 'Novmebro', 'Dezembro'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Relatórios',
+            data: [12, 19, 3, 5, 2, 3, 10, 9, 5, 11, 9, 5],
             backgroundColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                '#464362', '#464362', '#464362', '#464362','#464362', '#464362',
+                '#464362', '#464362', '#464362', '#464362', '#464362', '#464362'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'none'
             ],
             borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
+        }, {
+            label: 'Demandas',
+            data: [5, 12, 5, 12, 5, 1, 2, 6, 3, 10, 9, 2],
+            backgroundColor: [
+                '#ABA7CC', '#ABA7CC', '#ABA7CC', '#ABA7CC','#ABA7CC', '#ABA7CC',
+                '#ABA7CC', '#ABA7CC', '#ABA7CC', '#ABA7CC', '#ABA7CC', '#ABA7CC'
+            ],
+            borderColor: [
+                'none'
+            ],
+            borderWidth: 1
         }
-    }
-});
-
-var ctx = document.getElementById('myChart2').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-
-var ctx = document.getElementById('myChart3').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+        ]
     },
     options: {
         scales: {
